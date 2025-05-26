976515fdb25eb0fd783c3c94ed77d8e9a02fe6cb
“Arpita <arpitakumari10653@gmail.com>
2025-05-26 20:50:19 +0530

    add 2 line

--- 2_dict.py
+++ 2_dict.py
@@ -11,3 +11,9 @@ print(marks.values()) #it print value of the marks like 90 ,100,happy etc.
 
 marks.update({"Arpit": 89 , "sika": 8}) # that show dictionary is mutable, we can add anything and it will change in main dict. 
 print(marks) 
+
+
+print(marks.get("Ram2")) #print none
+print(marks.get["Ram2"])  # returns  an error both are not same 
+
+#both give same results when "Ram" and print 100 so for checking there is difference b/w them we do error and wrote "Ram2" for different result.
